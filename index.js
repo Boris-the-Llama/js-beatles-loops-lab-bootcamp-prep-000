c7b9719fbd6979f5fc0d4a3f9bd83e811a1ae5da
Dan Brett <boristhellama99@gmail.com>
2018-01-11 20:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
     var empty = [];
     do {
-      
-    }
+      empty.push("I love the Beatles!");
+      number++;
+    } while (number < 15);
 }
